fb00545460ea72ef38936857082f16b9a4e13257
Rakesh Annam <rakeshannam1998@gmail.com>
2026-04-29 11:55:28 +0530

    updated code in payment

--- physiotherapydoctor-service/src/main/java/physiotherapydoctor/controller/PaymentController.java
+++ physiotherapydoctor-service/src/main/java/physiotherapydoctor/controller/PaymentController.java
@@ -149,4 +149,24 @@ public class PaymentController {
 
         return ResponseEntity.status(response.getStatus()).body(response);
     }
+    @GetMapping("/payment/getExerciseSessionsWithRecords/{clinicId}/{branchId}/{bookingId}/{patientId}/{therapistRecordId}/{exerciseId}")
+    public ResponseEntity<Response> getExerciseSessionsWithRecords(
+            @PathVariable String clinicId,
+            @PathVariable String branchId,
+            @PathVariable String bookingId,
+            @PathVariable String patientId,
+            @PathVariable String therapistRecordId,
+            @PathVariable String exerciseId) {
+
+        Response response = service.getExerciseSessionsWithRecords(
+                clinicId,
+                branchId,
+                bookingId,
+                patientId,
+                therapistRecordId,
+                exerciseId
+        );
+
+        return ResponseEntity.ok(response);
+    }
 }
